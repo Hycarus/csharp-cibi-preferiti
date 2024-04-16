939ac6c7581d704811d7db9db0709c06c47e2803
Hycarus <mirkomarasco08@gmail.com>
2024-04-16 14:40:33 +0200

    main exercise tasks

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] favouriteFoods = { "Torta alle mele", "Lasagne", "Pollo fritto", "Sushi", "Patatine", "Pizza" };
+        Console.WriteLine($"Lunghezza della Classifica: {favouriteFoods.Length}");
+
+        for(int i = 0; i < favouriteFoods.Length; i++)
+        {
+            Console.WriteLine($"{i + 1} {favouriteFoods[i]}");
+        }
+
+        Console.WriteLine($"Cibo preferito: {favouriteFoods[0]}");
+
+        Console.WriteLine($"Cibo preferito (ma non troppo): {favouriteFoods[favouriteFoods.Length - 1]}");
     }
 }
 
